96b2ff294f43d242c9e7b31b37d8a3aea033ad53
Dell <2720852268@qq.com>
2020-12-19 18:07:04 +0800

    测试上传

---
+++ src/com/bjpowernode/domain/Student.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.domain;
+
+public class Student {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
